9d80b7ed8ca1386b39b5a6db801a48e122aa435c
vesafetahaj <vf58160@ubt-uni.net>
2022-12-11 16:20:42 +0100

    commit

---
+++ javascript/home.js
@@ -0,0 +1,13 @@
+var pic = document.querySelector("img");
+function showPht1(){
+    pic.style.marginLeft = "0";
+}
+function showPht2(){
+     pic.style.marginLeft = "-25%";
+}
+function showPht3(){
+     pic.style.marginLeft = "-50%";
+}
+function showPht4(){
+     pic.style.marginLeft = "-75%";
+}
