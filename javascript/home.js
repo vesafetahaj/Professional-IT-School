a72a52dbd1b389e08a0cf100078540ca234abd5b
vesafetahaj <vf58160@ubt-uni.net>
2022-12-14 16:56:40 +0100

    commit

--- javascript/home.js
+++ javascript/home.js
@@ -16,4 +16,21 @@ function showSlides() {
   setTimeout(showSlides, 2000);
 }
 
+  var button = document.getElementById('darkmode');
+	var text = document.getElementById('text');
+	var body = document.querySelector("body");
+  var aboutContent = document.getElementById("about-content");
+  var header = document.getElementById("header");
 
+button.onclick = function(){
+  body.style.backgroundColor = "black";
+  header.style.backgroundColor = "black";
+  body.style.color = "white";
+  aboutContent.style.boxShadow = "0 4px 8px 0 rgba(255, 255, 255, 0.2), 0 6px 20px 0 rgba(255, 255, 255, 0.19)";
+}
+button.ondblclick = function(){
+  body.style.backgroundColor = "white";
+  body.style.color = "black";
+  header.style.backgroundColor = "white";
+  aboutContent.style.boxShadow = "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)";
+}
